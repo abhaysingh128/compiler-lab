e93042b6dff392218cc93a6186c03d062b503665
*abhaysingh128* <*abhaysinghbisht2003@gmail.com*>
2025-06-18 20:49:13 +0530

    updated code

--- dfa/lex.yy.c
+++ dfa/lex.yy.c
@@ -282,13 +282,17 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 19
-#define YY_END_OF_BUFFER 20
-static yyconst short int yy_accept[31] =
+#define YY_NUM_RULES 43
+#define YY_END_OF_BUFFER 44
+static yyconst short int yy_accept[67] =
     {   0,
         0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
-       20,    4,    3,    1,    2,    8,    7,    5,    6,   12,
-       11,    9,   10,   16,   15,   13,   14,   17,   18,    0
+        0,    0,    0,    0,    0,    0,    0,    0,    0,    0,
+        0,    0,   44,    4,    3,    1,    2,    8,    7,    5,
+        6,   12,   11,    9,   10,   16,   15,   13,   14,   20,
+       19,   17,   18,   24,   23,   21,   22,   28,   27,   25,
+       26,   32,   31,   29,   30,   36,   35,   33,   34,   40,
+       39,   37,   38,   42,   41,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -328,34 +332,44 @@ static yyconst int yy_meta[5] =
         1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[32] =
+static yyconst short int yy_base[68] =
     {   0,
-        0,    0,    4,    0,    8,    0,   12,    0,   18,   17,
-       18,   21,   21,   21,   21,   21,   21,   21,   21,   21,
-       21,   21,   21,   21,   21,   21,   21,   21,   21,   21,
-       16
+        0,    0,    4,    0,    8,    0,   12,    0,   16,    0,
+       20,    0,   24,    0,   28,    0,   32,    0,   36,    0,
+       42,   41,   42,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   40
     } ;
 
-static yyconst short int yy_def[32] =
+static yyconst short int yy_def[68] =
     {   0,
-       30,    1,   30,    3,   30,    5,   30,    7,   31,   31,
-       30,   30,   30,   30,   30,   30,   30,   30,   30,   30,
-       30,   30,   30,   30,   30,   30,   30,   30,   30,    0,
-       30
+       66,    1,   66,    3,   66,    5,   66,    7,   66,    9,
+       66,   11,   66,   13,   66,   15,   66,   17,   66,   19,
+       67,   67,   66,   66,   66,   66,   66,   66,   66,   66,
+       66,   66,   66,   66,   66,   66,   66,   66,   66,   66,
+       66,   66,   66,   66,   66,   66,   66,   66,   66,   66,
+       66,   66,   66,   66,   66,   66,   66,   66,   66,   66,
+       66,   66,   66,   66,   66,    0,   66
     } ;
 
-static yyconst short int yy_nxt[26] =
+static yyconst short int yy_nxt[50] =
     {   0,
-       12,   13,   14,   15,   16,   17,   18,   19,   20,   21,
-       22,   23,   24,   25,   26,   27,   28,   30,   29,   29,
-       11,   30,   30,   30,   30
+       24,   25,   26,   27,   28,   29,   30,   31,   32,   33,
+       34,   35,   36,   37,   38,   39,   40,   41,   42,   43,
+       44,   45,   46,   47,   48,   49,   50,   51,   52,   53,
+       54,   55,   56,   57,   58,   59,   60,   61,   62,   63,
+       64,   66,   65,   65,   23,   66,   66,   66,   66
     } ;
 
-static yyconst short int yy_chk[26] =
+static yyconst short int yy_chk[50] =
     {   0,
         1,    1,    1,    1,    3,    3,    3,    3,    5,    5,
-        5,    5,    7,    7,    7,    7,   31,   11,   10,    9,
-       30,   30,   30,   30,   30
+        5,    5,    7,    7,    7,    7,    9,    9,    9,    9,
+       11,   11,   11,   11,   13,   13,   13,   13,   15,   15,
+       15,   15,   17,   17,   17,   17,   19,   19,   19,   19,
+       67,   23,   22,   21,   66,   66,   66,   66,   66
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -369,16 +383,22 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "dfa2.l"
+#line 1 "dfa5.l"
 #define INITIAL 0
-#line 2 "dfa2.l"
-#include<stdio.h>
-#define S0 1
-#define S1 2
-#define S2 3
-#define DEAD 4
+#line 2 "dfa5.l"
+#include <stdio.h>
+#define q0p0 1
+#define q0p1 2
+#define q0p2 3
+#define q1p0 4
+#define q1p1 5
+#define q1p2 6
+#define q2p0 7
+#define q2p1 8
+#define q2p2 9
+#define DEAD 10
 
-#line 382 "lex.yy.c"
+#line 402 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -529,9 +549,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 5 "dfa2.l"
+#line 7 "dfa5.l"
 
-#line 535 "lex.yy.c"
+#line 555 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -582,13 +602,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 31 )
+				if ( yy_current_state >= 67 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 21 );
+		while ( yy_base[yy_current_state] != 45 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -616,104 +636,230 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "dfa2.l"
-BEGIN S0;
+#line 8 "dfa5.l"
+BEGIN q1p0;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "dfa2.l"
-BEGIN S1;
+#line 9 "dfa5.l"
+BEGIN q0p1;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "dfa2.l"
-printf("Accepted (even a and even b)\n"); BEGIN INITIAL;
+#line 10 "dfa5.l"
+printf("Accepted\n"); BEGIN INITIAL;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 9 "dfa2.l"
+#line 11 "dfa5.l"
 BEGIN DEAD;
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 11 "dfa2.l"
-BEGIN INITIAL;
+#line 13 "dfa5.l"
+BEGIN q1p0;
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 12 "dfa2.l"
-BEGIN S2;
+#line 14 "dfa5.l"
+BEGIN q0p1;
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 13 "dfa2.l"
-printf("not accepted (odd a)\n"); BEGIN INITIAL;
+#line 15 "dfa5.l"
+printf("Accepted\n"); BEGIN INITIAL;
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 14 "dfa2.l"
+#line 16 "dfa5.l"
 BEGIN DEAD;
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 16 "dfa2.l"
-BEGIN S2;
+#line 18 "dfa5.l"
+BEGIN q1p1;
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 17 "dfa2.l"
-BEGIN INITIAL;
+#line 19 "dfa5.l"
+BEGIN q0p2;
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 18 "dfa2.l"
-printf("not accepted (odd b)\n"); BEGIN INITIAL;
+#line 20 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 19 "dfa2.l"
+#line 21 "dfa5.l"
 BEGIN DEAD;
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 21 "dfa2.l"
-BEGIN S1;
+#line 23 "dfa5.l"
+BEGIN q1p2;
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 22 "dfa2.l"
-BEGIN S0;
+#line 24 "dfa5.l"
+BEGIN q0p0;
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 23 "dfa2.l"
-printf("not accepted (odd a and odd b)\n"); BEGIN INITIAL;
+#line 25 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 24 "dfa2.l"
+#line 26 "dfa5.l"
 BEGIN DEAD;
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 26 "dfa2.l"
-BEGIN DEAD;
+#line 28 "dfa5.l"
+BEGIN q2p0;
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 27 "dfa2.l"
-printf("invalid string\n"); BEGIN INITIAL;
+#line 29 "dfa5.l"
+BEGIN q1p1;
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 28 "dfa2.l"
+#line 30 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 20:
+YY_RULE_SETUP
+#line 31 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 21:
+YY_RULE_SETUP
+#line 33 "dfa5.l"
+BEGIN q2p1;
+	YY_BREAK
+case 22:
+YY_RULE_SETUP
+#line 34 "dfa5.l"
+BEGIN q1p2;
+	YY_BREAK
+case 23:
+YY_RULE_SETUP
+#line 35 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 24:
+YY_RULE_SETUP
+#line 36 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 25:
+YY_RULE_SETUP
+#line 38 "dfa5.l"
+BEGIN q2p2;
+	YY_BREAK
+case 26:
+YY_RULE_SETUP
+#line 39 "dfa5.l"
+BEGIN q1p0;
+	YY_BREAK
+case 27:
+YY_RULE_SETUP
+#line 40 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 28:
+YY_RULE_SETUP
+#line 41 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 29:
+YY_RULE_SETUP
+#line 43 "dfa5.l"
+BEGIN q0p0;
+	YY_BREAK
+case 30:
+YY_RULE_SETUP
+#line 44 "dfa5.l"
+BEGIN q2p1;
+	YY_BREAK
+case 31:
+YY_RULE_SETUP
+#line 45 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 32:
+YY_RULE_SETUP
+#line 46 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 33:
+YY_RULE_SETUP
+#line 48 "dfa5.l"
+BEGIN q0p1;
+	YY_BREAK
+case 34:
+YY_RULE_SETUP
+#line 49 "dfa5.l"
+BEGIN q2p2;
+	YY_BREAK
+case 35:
+YY_RULE_SETUP
+#line 50 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 36:
+YY_RULE_SETUP
+#line 51 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 37:
+YY_RULE_SETUP
+#line 53 "dfa5.l"
+BEGIN q0p2;
+	YY_BREAK
+case 38:
+YY_RULE_SETUP
+#line 54 "dfa5.l"
+BEGIN q2p0;
+	YY_BREAK
+case 39:
+YY_RULE_SETUP
+#line 55 "dfa5.l"
+printf("Not Accepted\n"); BEGIN INITIAL;
+	YY_BREAK
+case 40:
+YY_RULE_SETUP
+#line 56 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 41:
+YY_RULE_SETUP
+#line 58 "dfa5.l"
+printf("Invalid\n"); BEGIN INITIAL;
+	YY_BREAK
+case 42:
+YY_RULE_SETUP
+#line 59 "dfa5.l"
+BEGIN DEAD;
+	YY_BREAK
+case 43:
+YY_RULE_SETUP
+#line 60 "dfa5.l"
 ECHO;
 	YY_BREAK
-#line 713 "lex.yy.c"
+#line 853 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
-case YY_STATE_EOF(S0):
-case YY_STATE_EOF(S1):
-case YY_STATE_EOF(S2):
+case YY_STATE_EOF(q0p0):
+case YY_STATE_EOF(q0p1):
+case YY_STATE_EOF(q0p2):
+case YY_STATE_EOF(q1p0):
+case YY_STATE_EOF(q1p1):
+case YY_STATE_EOF(q1p2):
+case YY_STATE_EOF(q2p0):
+case YY_STATE_EOF(q2p1):
+case YY_STATE_EOF(q2p2):
 case YY_STATE_EOF(DEAD):
 	yyterminate();
 
@@ -1005,7 +1151,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 31 )
+			if ( yy_current_state >= 67 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1040,11 +1186,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 31 )
+		if ( yy_current_state >= 67 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 30);
+	yy_is_jam = (yy_current_state == 66);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1599,14 +1745,14 @@ int main()
 	return 0;
 	}
 #endif
-#line 28 "dfa2.l"
+#line 60 "dfa5.l"
 
-int yywrap(){
-return 1;
+
+int yywrap() {
+    return 1;
 }
 
-int main(){
-printf("Enter a string\n");
-yylex();
-return 0;
+int main() {
+    yylex();
+    return 0;
 }
